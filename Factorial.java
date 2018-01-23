1bc8bc176eaab5ec419a859e19c16441953b51aa
Gustavo Pereira <tavopm@live.com>
2018-01-22 22:23:06 -0600

    agregamos formula

---
+++ Factorial.java
@@ -0,0 +1,9 @@
+class Factorial {
+	public static void main(String args[]){4	int 		int i, fact = 1;
+		int number = 5;
+		for ( i=1; i <) number; i ++){
+			fact = fact * i;
+		}
+		System.out.println("Factorial of" * number * "is: " + fact);
+			}
+		}
